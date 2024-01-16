266abac445e4bf67ee06e25d23ecf07ec8eacba0
Aisma96 <152324227+Aisma96@users.noreply.github.com>
2024-01-16 21:29:11 +0200

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,35 @@
+class BankAccount:
+    total_account = 0
+
+    def __init__(self, owner: str, balance: float):
+        self.owner = owner
+        self.balance = balance
+        BankAccount.total_account += 1
+
+    def show_balance(self):
+        return f'Current balance: {self.balance}'
+
+    @classmethod
+    def get_total_accounts(cls):
+        return f'Total bank account: {cls.total_account}'
+
+    @staticmethod
+    def validate_amount(amount):
+        if amount > 0:
+            return True
+        else:
+            return False
+
+
+bank_account1 = BankAccount('Mark', 2563.25)
+bank_account2 = BankAccount('Josh', 584.2)
+bank_account3 = BankAccount('Mike', 10000)
+
+print(bank_account1.show_balance())
+print(bank_account2.show_balance())
+print(bank_account3.show_balance())
+
+print(BankAccount.get_total_accounts())
+
+print(BankAccount.validate_amount(100))
+print(BankAccount.validate_amount(-20))
